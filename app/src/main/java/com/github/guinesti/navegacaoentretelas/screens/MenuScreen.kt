1bca40c231569e89f3be195914c47b8a50863ff1
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-24 19:52:42 -0300

    Passagem de múltiplos parâmetros nas telas de Perfil e Pedidos

--- app/src/main/java/com/github/guinesti/navegacaoentretelas/screens/MenuScreen.kt
+++ app/src/main/java/com/github/guinesti/navegacaoentretelas/screens/MenuScreen.kt
@@ -42,7 +42,7 @@ fun MenuScreen(modifier: Modifier = Modifier, navController: NavController) {
                 .align(Alignment.Center)
         ) {
             Button(
-                onClick = { navController.navigate("perfil") },
+                onClick = { navController.navigate("perfil/Cristiano Ronaldo/41") },
                 colors = ButtonDefaults.buttonColors(Color.White),
                 modifier = Modifier.size(width = 200.dp, height = 48.dp)
             ) {
@@ -54,7 +54,7 @@ fun MenuScreen(modifier: Modifier = Modifier, navController: NavController) {
             }
             Spacer(modifier = Modifier.height(16.dp))
             Button(
-                onClick = { navController.navigate("pedidos") },
+                onClick = { navController.navigate("pedidos?cliente=Cristiano Ronaldo") },
                 colors = ButtonDefaults.buttonColors(Color.White),
                 modifier = Modifier.size(width = 200.dp, height = 48.dp)
             ) {
--- app/src/main/java/com/github/guinesti/navegacaoentretelas/screens/PedidosScreen.kt
+++ app/src/main/java/com/github/guinesti/navegacaoentretelas/screens/PedidosScreen.kt
@@ -18,14 +18,14 @@ import androidx.navigation.NavController
 
 
 @Composable
-fun PedidosScreen(modifier: Modifier = Modifier, navController: NavController) {
+fun PedidosScreen(modifier: Modifier = Modifier, navController: NavController, cliente: String?) {
     Box(modifier = modifier
         .fillMaxSize()
         .background(Color(0xFFFA9A9A9))
         .padding(32.dp)
     ) {
         Text(
-            text = "PEDIDOS",
+            text = "PEDIDOS - $cliente",
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
             color = Color.White
